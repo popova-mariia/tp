0f171ea8c1973a811504bc65da2b77483984d03c
moingshoing <yapsuperman@gmail.com>
2025-03-24 16:23:24 +0800

    Update arguments in Person object

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -53,7 +53,7 @@ public class AddCommandParser implements Parser<AddCommand> {
         Remark remark = new Remark(""); // add command does not allow adding remarks straight away
         Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
 
-        Person person = new Person(name, phone, address, gender, remark, tagList);
+        Person person = new Person(name, phone, address, gender, appointmentDate, remark, tagList);
 
         return new AddCommand(person);
     }
