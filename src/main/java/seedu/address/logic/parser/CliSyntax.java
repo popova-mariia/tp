028456702115e2d9cf3ecc9cb84b2ef93387e480
niha81 <nihadolly@gmail.com>
2025-03-24 18:18:17 +0800

    Add prefixes

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -10,9 +10,9 @@ public class CliSyntax {
     public static final Prefix PREFIX_PHONE = new Prefix("-p ");
     public static final Prefix PREFIX_ADDRESS = new Prefix("-a ");
     public static final Prefix PREFIX_GENDER = new Prefix("-g ");
-
-    // reminder to remove below prefixes if not necessary
     public static final Prefix PREFIX_REMARK = new Prefix("r/");
     public static final Prefix PREFIX_TAG = new Prefix("-t");
+    public static final Prefix PREFIX_CONDITION = new Prefix("-c");
+    public static final Prefix PREFIX_DETAILS = new Prefix("-d");
 
 }
