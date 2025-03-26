84c58bb95d8dbd80adfa4e15cbeea7b0f7abfc67
engulfy <xhaoyux@gmail.com>
2025-03-27 01:20:19 +0800

    Fix a small error

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -55,9 +55,7 @@ public class PersonUtil {
                 .append(appointmentDate.value).append(" "));
         if (descriptor.getTags().isPresent()) {
             Set<Tag> tags = descriptor.getTags().get();
-            if (tags.isEmpty()) {
-                sb.append(PREFIX_TAG);
-            } else {
+            if (!tags.isEmpty()) {
                 tags.forEach(s -> sb.append(PREFIX_TAG).append(s.tagName).append(" "));
             }
         }
