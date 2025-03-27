be13debb93d2925ef3840d3e897cd96e77e4ddd3
patil-np <nidhi.npatil@outlook.com>
2025-03-27 19:11:21 +0800

    Clean up

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -110,19 +110,6 @@ public class MainWindow extends UiPart<Stage> {
     /**
      * Fills up all the placeholders of this window.
      */
-    //    void fillInnerParts() {
-    //        personListPanel = new PersonListPanel(logic.getFilteredPersonList());
-    //        personListPanelPlaceholder.getChildren().add(personListPanel.getRoot());
-    //
-    //        resultDisplay = new ResultDisplay();
-    //        resultDisplayPlaceholder.getChildren().add(resultDisplay.getRoot());
-    //
-    //        StatusBarFooter statusBarFooter = new StatusBarFooter(logic.getAddressBookFilePath());
-    //        statusbarPlaceholder.getChildren().add(statusBarFooter.getRoot());
-    //
-    //        CommandBox commandBox = new CommandBox(this::executeCommand);
-    //        commandBoxPlaceholder.getChildren().add(commandBox.getRoot());
-    //    }
     void fillInnerParts() {
         personListPanel = new PersonListPanel(
                 logic.getFilteredPersonList(),
