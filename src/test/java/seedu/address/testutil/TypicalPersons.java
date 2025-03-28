88bc64a3595a4a77d60ac7e32e146abeec36fdbc
moingshoing <yapsuperman@gmail.com>
2025-03-28 19:39:00 +0800

    Revert changes for TypicalPersons

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -4,16 +4,14 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_APPT_DATE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_APPT_DATE_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_CONDITION_ECZEMA;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_DETAIL_ALONE;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_DETAIL_MORNING;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_GENDER_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_GENDER_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_CONDITION_DEMENTIA;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -30,17 +28,17 @@ public class TypicalPersons {
     public static final Person ALICE = new PersonBuilder().withName("Alice Pauline")
             .withAddress("123, Jurong West Ave 6, #08-111")
                 .withPhone("94351253").withRemark("She likes aardvarks.").withGender("female")
-                .withAppointmentDate("2025-10-10").withConditions("dementia").build();
+                .withAppointmentDate("2025-10-10").withTags("friends").build();
     public static final Person BENSON = new PersonBuilder().withName("Benson Meier")
             .withAddress("311, Clementi Ave 2, #02-25").withRemark("He can't take beer!")
                 .withPhone("98765432").withGender("male")
-                .withAppointmentDate("2025-01-10 10:30").withConditions("dementia", "eczema").build();
+                .withAppointmentDate("2025-01-10 10:30").withTags("owesMoney", "friends").build();
     public static final Person CARL = new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
             .withAddress("wall street").withGender("female")
             .withAppointmentDate("2025-11-10 11:30").build();
     public static final Person DANIEL = new PersonBuilder().withName("Daniel Meier").withPhone("87652533")
-            .withAddress("10th street").withGender("male")
-            .withConditions("dementia").withAppointmentDate("2025-12-05 09:00").build();
+            .withAddress("10th street")
+                .withGender("male").withTags("friends").withAppointmentDate("2025-12-05 09:00").build();
     public static final Person ELLE = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
             .withAddress("michegan ave").withGender("female").withAppointmentDate("2025-01-11").build();
     public static final Person FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
@@ -56,12 +54,11 @@ public class TypicalPersons {
 
     // Manually added - Person's details found in {@code CommandTestUtil}
     public static final Person AMY = new PersonBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY)
-            .withAddress(VALID_ADDRESS_AMY).withGender(VALID_GENDER_AMY).withAppointmentDate(VALID_APPT_DATE_AMY)
-            .withConditions(VALID_CONDITION_DEMENTIA).withDetails(VALID_DETAIL_ALONE).build();
+            .withAddress(VALID_ADDRESS_AMY).withGender(VALID_GENDER_AMY)
+                .withAppointmentDate(VALID_APPT_DATE_AMY).withTags(VALID_TAG_FRIEND).build();
     public static final Person BOB = new PersonBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
-            .withAddress(VALID_ADDRESS_BOB).withGender(VALID_GENDER_BOB).withAppointmentDate(VALID_APPT_DATE_BOB)
-            .withConditions(VALID_CONDITION_DEMENTIA, VALID_CONDITION_ECZEMA)
-            .withDetails(VALID_DETAIL_ALONE, VALID_DETAIL_MORNING).build();
+            .withAddress(VALID_ADDRESS_BOB).withGender(VALID_GENDER_BOB)
+                .withAppointmentDate(VALID_APPT_DATE_BOB).withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND).build();
 
     public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
 
