60dd8dc1c501b5bd20565edebb580fd80e39a419
popova-mariia <tomashapopova@gmail.com>
2025-03-29 16:07:23 +0800

    Fix AddressBookParserTest.java

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -58,7 +58,7 @@ public class AddressBookParserTest {
 
     @Test
     public void parseCommand_edit() throws Exception {
-        Person person = new PersonBuilder().withDetails("sample").build();
+        Person person = new PersonBuilder().withConditions("sample").build();
 
         EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(person).build();
 
