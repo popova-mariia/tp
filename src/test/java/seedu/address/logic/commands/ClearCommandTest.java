eb311d2373f005ca3a3998ef28f4a64d08520c77
popova-mariia <tomashapopova@gmail.com>
2025-03-31 16:55:59 +0800

    Update ClearCommandTest.java

--- src/test/java/seedu/address/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearCommandTest.java
@@ -3,6 +3,7 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
 
 public class ClearCommandTest {
 
@@ -25,6 +27,19 @@ public class ClearCommandTest {
     public void execute_nonEmptyAddressBook_setsClearPending() {
         Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
+        CommandResult result = new ClearCommand().execute(model);
+
+        assertEquals(ClearCommand.MESSAGE_CONFIRMATION, result.getFeedbackToUser());
+        assertTrue(model.isClearPending());
+        assertEquals(getTypicalAddressBook(), model.getAddressBook()); // nothing actually cleared yet
+    }
+
+    @Test
+    public void execute_nonEmptyAddressBook_setsClearPendingWhenDeleteAlreadyPending() {
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+        Person personToDelete = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        DeleteCommand deleteCommand = new DeleteCommand(INDEX_FIRST_PERSON);
         CommandResult result = new ClearCommand().execute(model);
 
         assertEquals(ClearCommand.MESSAGE_CONFIRMATION, result.getFeedbackToUser());
