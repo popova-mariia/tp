3d83faff0198ccbd1b586c551c53328a5444737b
moingshoing <yapsuperman@gmail.com>
2025-04-02 17:06:08 +0800

    Update model to get list instead of filtered list

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -102,7 +102,7 @@ public class EditCommand extends Command {
         Person editedPerson = createEditedPerson(personToEdit, editPersonDescriptor);
 
         if (!personToEdit.isSamePerson(editedPerson)
-                && model.getFilteredPersonList().stream()
+                && model.getAddressBook().getPersonList().stream()
                 .anyMatch(p -> !p.equals(personToEdit) && p.isSamePerson(editedPerson))) {
             throw new CommandException(MESSAGE_DUPLICATE_PERSON);
         }
