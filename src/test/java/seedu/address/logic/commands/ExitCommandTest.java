6163cd80a8c59b3df4848e8308d48f73cd29b392
patil-np <nidhi.npatil@outlook.com>
2025-04-02 22:21:39 +0800

    tests for exit command

--- src/test/java/seedu/address/logic/commands/ExitCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExitCommandTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.PersonBuilder;
 
 public class ExitCommandTest {
     private Model model = new ModelManager();
@@ -16,5 +18,25 @@ public class ExitCommandTest {
     public void execute_exit_success() {
         CommandResult expectedCommandResult = new CommandResult(MESSAGE_EXIT_ACKNOWLEDGEMENT, false, true);
         assertCommandSuccess(new ExitCommand(), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_clearsClearPendingFlag() {
+        model.setPendingClear();
+
+        CommandResult expectedResult = new CommandResult(MESSAGE_EXIT_ACKNOWLEDGEMENT, false, true);
+        assertCommandSuccess(new ExitCommand(), model, expectedResult, expectedModel);
+    }
+
+    @Test
+    public void execute_clearsDeletePendingFlag() {
+        Person person = new PersonBuilder().build();
+        model.addPerson(person);
+        model.setPendingDeletion(person);
+
+        expectedModel.addPerson(person);
+
+        CommandResult expectedResult = new CommandResult(MESSAGE_EXIT_ACKNOWLEDGEMENT, false, true);
+        assertCommandSuccess(new ExitCommand(), model, expectedResult, expectedModel);
     }
 }
