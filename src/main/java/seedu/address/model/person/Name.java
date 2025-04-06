e6c76a0253ad7eaf772ff64545fac7cc3cf15403
niha81 <nihadolly@gmail.com>
2025-04-06 20:59:13 +0800

    Add special characters and limit name length

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -10,13 +10,13 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Name {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names should only contain alphanumeric characters and spaces, and it should not be blank";
+            "Names can include 1 to 50 characters of alphanumerics, spaces, '-', '.', '@' and apostrophes.";
 
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}/@.'\\- ]{1,50}";
 
     public final String fullName;
 
